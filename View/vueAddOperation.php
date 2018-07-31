8d96a04972c8b825da8f143002b3b6f1568af4e6
Maxime <maximebarber@gmail.com>
2018-07-31 19:55:55 +0200

    Ajout commentaires

--- View/vueAddOperation.php
+++ View/vueAddOperation.php
@@ -5,18 +5,22 @@
 <!-- Formulaire d'ajout d'une opération -->
 <form method="POST">
 
+    <!-- Champ date -->
     <div class="form-group">
         <input type="date" class="form-control" name="date">
     </div>
 
+    <!-- Champ libellé -->
     <div class="form-group">
         <input type="text" class="form-control" placeholder="Libellé de l'opération" name="libelle">
     </div>
 
+    <!-- Champ montant -->
     <div class="form-group">
         <input type="number" step="any" class="form-control" placeholder="Montant de l'opération" name="montant">
     </div>
 
+    <!-- Liste déroulante types -->
     <div class="form-group">
         <label for="type">Type</label>
         <select class="form-control" id="type" name="id_type">
@@ -29,6 +33,7 @@
         </select>
     </div>
 
+    <!-- Liste déroulante catégories -->
     <div class="form-group">
         <label for="categorie">Catégorie</label>
         <select class="form-control" id="categorie" name="id_categorie">
@@ -41,6 +46,7 @@
         </select>
     </div>
 
+    <!-- Liste déroulante comptes bancaires -->
     <div class="form-group">
         <label for="categorie">Compte Bancaire</label>
         <select class="form-control" id="categorie" name="id_compte_bancaire">
@@ -53,6 +59,7 @@
         </select>
     </div>
 
+    <!-- Boutons radio Crédit ou Débit -->
     <div class="form-group">
         <div class="form-check form-check-inline">
             <input class="form-check-input" type="radio" name="nature" id="credit" value="C">
@@ -64,6 +71,7 @@
         </div>
     </div>
 
+    <!-- Checkbox dépense fixe -->
     <div class="form-group form-check">
         <input type="hidden" value="0" name="fixe">
         <input type="checkbox" class="form-check-input" id="fixe" name="fixe" value="1">
