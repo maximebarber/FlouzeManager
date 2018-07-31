69980a673d515c8ae399406093ef4e4cd81a029f
Maxime <maximebarber@gmail.com>
2018-07-31 13:56:53 +0200

    Application template à la vueOperation

--- View/vueOperations.php
+++ View/vueOperations.php
@@ -1,55 +1,51 @@
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="author" content="" />
-        <meta name="description" content="" />
-        <title>Opérations</title>
-    </head>
-    <body>
+<?php $titre = 'Opérations' ?>
 
-        <!-- Affiche le récapitulaif des opérations -->
-        <div class="contenu">
+<?php ob_start(); ?>
 
-            <table border="1">
-                <thead>
-                    <th>Date</th>
-                    <th>Libellé</th>
-                    <th>Type</th>
-                    <th>Catégorie</th>
-                    <th>Débit</th>
-                    <th>Crédit</th>
-                </thead>
+<!-- Affiche le récapitulaif des opérations -->
+<div class="contenu">
 
-                <!-- Récupère les données stockées dans la BDD -->
-                <?php foreach ($operations as $operation): ?>
-                
-                    <tr>
-                        
-                        <td><?= $operation['date_operation'] ?></td>
-                        <td><?= $operation['libelle_operation'] ?></td>
-                        <td><?= $operation['nom_type_operation'] ?></td>
-                        <td><?= $operation['nom_categorie_operation'] ?></td>
+    <table border="1">
+        <thead>
+        <th>Date</th>
+        <th>Libellé</th>
+        <th>Type</th>
+        <th>Catégorie</th>
+        <th>Débit</th>
+        <th>Crédit</th>
+        </thead>
 
-                        <!-- Affichage du montant de l'opération dans crédit ou débit -->
-                        <?php if ($operation['nature_operation'] === 'D') { ?>
+        <!-- Récupère les données stockées dans la BDD -->
+        <?php foreach ($operations as $operation): ?>
 
-                            <td>- <?= $operation['montant_operation'] ?> €</td>
-                            <td></td>
-                                
-                        <?php } else { ?>
-                            
-                            <td></td>
-                            <td>+ <?= $operation['montant_operation'] ?> €</td>
-                            
-                        <?php } ?>
-                            
-                    </tr>
-                    
-                <?php endforeach; ?>
+            <tr>
 
-            </table>
+                <td><?= $operation['date_operation'] ?></td>
+                <td><?= $operation['libelle_operation'] ?></td>
+                <td><?= $operation['nom_type_operation'] ?></td>
+                <td><?= $operation['nom_categorie_operation'] ?></td>
 
-        </div>
+                <!-- Affichage du montant de l'opération dans crédit ou débit -->
+                <?php if ($operation['nature_operation'] === 'D') { ?>
 
-    </body>
-</html>
+                    <td>- <?= $operation['montant_operation'] ?> €</td>
+                    <td></td>
+
+                <?php } else { ?>
+
+                    <td></td>
+                    <td>+ <?= $operation['montant_operation'] ?> €</td>
+
+                <?php } ?>
+
+            </tr>
+
+        <?php endforeach; ?>
+
+    </table>
+
+</div> <!-- .contenu -->
+
+<?php $contenu = ob_get_clean(); ?>
+
+<?php require 'template.php'; ?>
